2a26f1344a464a2bd5614518a022909d6f139bae
bala-amypo <bala@amypo.in>
2025-12-20 17:24:05 +0000

    User commit

---
+++ src/main/java/com/example/demo/entity/VerificationRequest.java
@@ -0,0 +1,45 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "verification_requests")
+public class VerificationRequest {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String status;
+
+    @ManyToOne
+    @JoinColumn(name = "credential_id")
+    private CredentialRecord credential;
+
+    public VerificationRequest() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+    
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+    
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public CredentialRecord getCredential() {
+        return credential;
+    }
+    
+    public void setCredential(CredentialRecord credential) {
+        this.credential = credential;
+    }
+}
