a62cc71cdf0845dfb68f4ea3074a1c29ae4dc44b
bala-amypo <bala@amypo.in>
2025-12-21 09:15:31 +0000

    User commit

--- src/main/java/com/example/demo/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -1,33 +1,22 @@
 package com.example.demo.config;
 
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.security.SecurityRequirement;
-import io.swagger.v3.oas.models.security.SecurityScheme;
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.servers.Server;
+
 @Configuration
-public class SwaggerConfig {
+public class OpenApiConfig {
 
-    @Bean
-    public OpenAPI openAPI() {
-
-        return new OpenAPI()
-                .info(new Info()
-                        .title("Digital Credential Verification Engine")
-                        .version("1.0")
-                        .description("Digital Credential Verification Engine APIs"))
-                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
-                .components(
-                        new io.swagger.v3.oas.models.Components()
-                                .addSecuritySchemes(
-                                        "bearerAuth",
-                                        new SecurityScheme()
-                                                .type(SecurityScheme.Type.HTTP)
-                                                .scheme("bearer")
-                                                .bearerFormat("JWT")
-                                )
-                );
-    }
+        @Bean
+        public OpenAPI customOpenAPI() {
+                return new OpenAPI()
+                        
+                        .servers(List.of(
+                        new Server().url("https://9015")
+                ));
+        }
 }
