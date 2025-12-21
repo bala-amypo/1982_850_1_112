d75e519b8c4a222f0eb66a5e2902aef5c307a6f8
bala-amypo <bala@amypo.in>
2025-12-21 07:44:04 +0000

    User commit

--- src/main/java/com/example/demo/security/JwtAuthenticationFilter.java
+++ src/main/java/com/example/demo/security/JwtAuthenticationFilter.java
@@ -1,5 +1,26 @@
 package com.example.demo.security;
 
-public class JwtAuthenticationFilter {
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class JwtAuthenticationEntryPoint
+        implements AuthenticationEntryPoint {
+
+    @Override
+    public void commence(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            AuthenticationException authException)
+            throws IOException, ServletException {
+
+        response.sendError(
+                HttpServletResponse.SC_UNAUTHORIZED,
+                "Unauthorized: Invalid or missing JWT token");
+    }
 }
