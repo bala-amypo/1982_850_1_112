51990ae5ba2ec348a779bea8a65041f1a0cd48dc
bala-amypo <bala@amypo.in>
2025-12-23 04:26:00 +0000

    User commit

--- src/main/java/com/example/demo/repository/CredentialHolderProfileRepository.java
+++ src/main/java/com/example/demo/repository/CredentialHolderProfileRepository.java
@@ -2,15 +2,11 @@ package com.example.demo.repository;
 
 import com.example.demo.entity.CredentialHolderProfile;
 import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
+import org.springframework.stereotype.Repository;
 
-public interface CredentialHolderProfileRepository extends JpaRepository<CredentialHolderProfile, Long> {
+@Repository
+public interface CredentialHolderProfileRepository
+        extends JpaRepository<CredentialHolderProfile, Long> {
 
-    Optional<CredentialHolderProfile> findById(Long id);
-
-    @SuppressWarnings("unchecked")
-    @Override
-    CredentialHolderProfile save(CredentialHolderProfile profile);
-
-    
+    // Add ONLY custom query methods here if needed
 }
