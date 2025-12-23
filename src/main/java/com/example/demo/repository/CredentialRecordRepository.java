291e1054a2e1d2c2758d6060dd40db9d23f72228
bala-amypo <bala@amypo.in>
2025-12-23 04:52:15 +0000

    User commit

--- src/main/java/com/example/demo/repository/CredentialRecordRepository.java
+++ src/main/java/com/example/demo/repository/CredentialRecordRepository.java
@@ -2,28 +2,14 @@ package com.example.demo.repository;
 
 import com.example.demo.entity.CredentialRecord;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
 import java.time.LocalDate;
 import java.util.List;
-import java.util.Optional;
 
-public interface CredentialRecordRepository extends JpaRepository<CredentialRecord, Long> {
-
-    @Override
-    CredentialRecord save(CredentialRecord record);
-
-    Optional<CredentialRecord> findById(CredentialRecord id);
-
-    List<CredentialRecord> findByHolderId(Long holderId);
-
-    Optional<CredentialRecord> findByCredentialCode(String credentialCode);
+@Repository
+public interface CredentialRecordRepository
+        extends JpaRepository<CredentialRecord, Long> {
 
     List<CredentialRecord> findByExpiryDateBefore(LocalDate date);
-
-
-    @Query("SELECT c FROM CredentialRecord c WHERE c.status = :status")
-    List<CredentialRecord> findByStatusUsingHql(String status);
-
-    @Query("SELECT c FROM CredentialRecord c WHERE c.issuer = :issuer AND c.credentialType = :credentialType")
-    List<CredentialRecord> searchByIssuerAndType(String issuer, String credentialType);
 }
