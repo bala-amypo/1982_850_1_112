3416391d673feaa7478a417c5514cee3d070a8e2
bala-amypo <bala@amypo.in>
2025-12-23 05:07:17 +0000

    User commit

--- src/main/java/com/example/demo/service/CredentialRecordService.java
+++ src/main/java/com/example/demo/service/CredentialRecordService.java
@@ -29,3 +29,14 @@ public class CredentialRecord {
 
     
 }
+package com.example.demo.service;
+
+import com.example.demo.entity.CredentialRecord;
+import java.util.List;
+
+public interface CredentialRecordService {
+
+    List<CredentialRecord> getByHolderId(Long holderId);
+
+    CredentialRecord getByCredentialCode(String code);
+}
