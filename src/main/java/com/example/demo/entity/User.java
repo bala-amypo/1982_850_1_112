0640e463b33a89896639d9154a35cf558ede4927
bala-amypo <bala@amypo.in>
2025-12-23 04:37:17 +0000

    User commit

--- src/main/java/com/example/demo/entity/User.java
+++ src/main/java/com/example/demo/entity/User.java
@@ -10,6 +10,7 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
     private String fullName;
 
     @Column(nullable = false, unique = true)
@@ -18,10 +19,10 @@ public class User {
     @Column(nullable = false)
     private String password;
 
+    @Column(nullable = false)
     private String role;
 
-    public User() {
-    }
+    // getters & setters
 
     public Long getId() {
         return id;
@@ -31,26 +32,26 @@ public class User {
         this.id = id;
     }
 
-    public String getFullname() {
+    public String getFullName() {
         return fullName;
     }
-    
-    public void setFullname(String fullname) {
-        this.fullName = fullname;
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
     }
-    public String getEmail(){
+
+    public String getEmail() {
         return email;
     }
 
-    public void setEmail(String email){
-        this.email= email;
+    public void setEmail(String email) {
+        this.email = email;
     }
-
 
     public String getPassword() {
         return password;
     }
-    
+
     public void setPassword(String password) {
         this.password = password;
     }
@@ -58,7 +59,7 @@ public class User {
     public String getRole() {
         return role;
     }
-    
+
     public void setRole(String role) {
         this.role = role;
     }
