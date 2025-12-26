c63a943bbb6013c8a6d57f8fb3dd06bb1b9a6223
bala-amypo <bala@amypo.in>
2025-12-26 05:17:04 +0000

    User commit

--- src/main/java/com/example/demo/repository/AuditTrailRecordRepository.java
+++ src/main/java/com/example/demo/repository/AuditTrailRecordRepository.java
@@ -5,9 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface AuditTrailRecordRepository
-        extends JpaRepository<AuditTrailRecord, Long> {
-
-    
-    List<AuditTrailRecord> findByCredentialIdOrderByLoggedAtDesc(Long credentialId);
+public interface AuditTrailRecordRepository extends JpaRepository<AuditTrailRecord, Long> {
+    List<AuditTrailRecord> findByCredentialId(Long credentialId);
 }
